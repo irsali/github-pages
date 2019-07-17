7fccf39f24de6b8c29a4b9efe885b4135c747820
Irshad Ali <text2irshad@gmail.com>
2019-07-17 20:07:18 +0530

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.github.com/',
   odataUrl: 'https://api.github.com/',
-  token: process.env.token
+  token: '3dd20abc01b1854d5db89f82368f45e582f4f708'
 };
